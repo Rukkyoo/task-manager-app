7249582506ca9955e1fc4167657cfd84f6b08a67
Michael Omonedo <rukkyomonedo@gmail.com>
2025-01-13 15:58:08 +0100

    added errors

--- tsk-man/backend/app.js
+++ tsk-man/backend/app.js
@@ -1,1 +1,22 @@
-console.log('Hello from app.js');
+import express from "express";
+const app = express();
+
+const port = 3000;
+
+console.log(`Hello from port ${port} in app.js`);
+
+const start = async () => {
+  try {
+    app.get("/hello", (req, res) => {
+      res.send("welcome to my task manager app");
+    });
+
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
