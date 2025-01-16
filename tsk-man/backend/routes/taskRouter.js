3df3722cac1deb72f75147f5440344dd2137a1c2
Michael Omonedo <rukkyomonedo@gmail.com>
2025-01-16 15:30:00 +0100

    finished controllers and routes

--- tsk-man/backend/routes/taskRouter.js
+++ tsk-man/backend/routes/taskRouter.js
@@ -2,10 +2,13 @@ import express from "express";
 const router = express.Router();
 import { getAllToDos } from "./../controllers/todo.js";
 import { createToDo } from "./../controllers/todo.js";
+import { getSingleToDo } from "./../controllers/todo.js";
+import { editToDo } from "./../controllers/todo.js";
+import { deleteToDo } from "./../controllers/todo.js";
 
-router.route("/").get(getAllToDos);
+router.route("/").get(getAllToDos).post(createToDo);
 
-router.route("/").post(createToDo);
+router.route("/:id").get(getSingleToDo).patch(editToDo).delete(deleteToDo);
 
 export default router;
 
